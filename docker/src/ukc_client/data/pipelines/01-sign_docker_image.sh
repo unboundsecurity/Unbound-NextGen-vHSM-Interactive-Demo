5b0a563b58b356b0b629ff9bb71ffbebf917fd19
Ori Ben Sasson <ori.ben-sasson@unboundtech.com>
2020-04-23 06:59:04 +0000

    Hide warnings

--- docker/src/ukc_client/data/pipelines/01-sign_docker_image.sh
+++ docker/src/ukc_client/data/pipelines/01-sign_docker_image.sh
@@ -2,10 +2,10 @@
 
 echo "#################################################################### Docker signer ####################################################################"
 echo "Load key to GPG keyring"
-ucl pgp-key -p $UKC_PARTITION -n codeSigning
+ucl pgp-key -p $UKC_PARTITION -n codeSigning 2> /dev/null
 
 echo "Export the public key"
-gpg2 --armor --output /certs/public.gpg --export codeSigning
+gpg2 --armor --output /certs/public.gpg --export codeSigning 2> /dev/null
 
 echo "Copy an image with skopeo"
 skopeo copy docker://busybox:latest dir:busybox
