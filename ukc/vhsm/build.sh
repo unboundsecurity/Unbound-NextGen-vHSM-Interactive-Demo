1714f03f3ee882e0fceb2fc8cee501c43d3facde
Adam Ilan <adam.ilan@unboundtech.com>
2020-10-07 13:09:43 +0300

    automate vhsm build script

--- ukc/vhsm/build.sh
+++ ukc/vhsm/build.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
+set -e
+
 version=${UNBOUND_VERSION:-2007}
 sh ../ukc-server/build.sh
+sh ./vhsm_webapp_build/build_web_app.sh
 sh ./ukc-server-vhsm/build.sh
 sh ./ukc-client-vhsm/build.sh
-
-
-## to build and create the webapp jar, you need to 
--- ukc/vhsm/vhsm_webapp_build/build_web_app.sh
+++ ukc/vhsm/vhsm_webapp_build/build_web_app.sh
@@ -3,17 +3,23 @@
 # kill running container
 docker rm -f vhsm-dev
 
-WEBAPP_ROOT=$(pwd)/webapp/
+SCRIPTPATH="$( cd "$(dirname "$0")" >/dev/null 2>&1 ; pwd -P )"
+echo ${SCRIPTPATH}
 
-# map to host folder to cache dependencies
+sh $SCRIPTPATH/build.sh
+
+WEBAPP_ROOT=$SCRIPTPATH/webapp/
+
+
+# map to host folder .m2 to cache dependencies
 docker run -d \
-  -v $WEBAPP_ROOT/../.m2/:/root/.m2/ \ 
-  -v $WEBAPP_ROOT:/unbound \
+  -v "$SCRIPTPATH/.m2/:/root/.m2/" \
+  -v "${WEBAPP_ROOT}:/unbound" \
   --name vhsm-dev \
   --network casp-docker_default \
   -p 127.0.0.1:35729:35729/tcp \
   -p 127.0.0.1:8081:8081/tcp -p 127.0.0.1:8444:8443 \
-  --env-file ../settings.env \
+  --env-file $(dirname "$0")/../settings.env \
   -e VHSM_DEMO_TLS_KEY_ALIAS=democlient3 \
   -e VHSM_DEMO_USE_HTTPS=false \
   ${UNBOUND_REPO:-unboundukc}/ukc-client:2007.vhsm.dev
@@ -28,9 +34,13 @@ docker exec  \
    -Dpackaging=jar \
    -DgeneratePom=true'
 
-# build the vhsm web app
+# build the vhsm web app and test by connecting to running vhsm-server
+# docker exec  \
+#      -w /setup -it vhsm-dev bash -c './start.sh; cd /unbound; mvn clean package;'
+
+# build the vhsm web app without testing - no need to have a running server
 docker exec  \
-     -w /setup -it vhsm-dev bash -c './start.sh; cd /unbound; mvn clean package;'
+     -w /setup -it vhsm-dev bash -c ' cd /unbound; mvn clean package -Dmaven.test.skip=true;'
 
 # copy jar to vhsm-client data folder
-cp $WEBAPP_ROOT/target/ukc-vhsm-server-0.0.1.jar ../ukc-client-vhsm/data
+cp $WEBAPP_ROOT/target/ukc-vhsm-server-0.0.1.jar $(dirname "$0")/../ukc-client-vhsm/data
