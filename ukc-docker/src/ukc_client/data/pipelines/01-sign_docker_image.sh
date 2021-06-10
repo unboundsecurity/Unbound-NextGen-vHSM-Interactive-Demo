c108763e8275519e0417ad0ed2cda9df26ae092d
Yuli <stremovsky@gmail.com>
2021-06-10 15:34:51 +0300

    fix skopeo copy command

--- ukc-docker/src/ukc_client/data/pipelines/01-sign_docker_image.sh
+++ ukc-docker/src/ukc_client/data/pipelines/01-sign_docker_image.sh
@@ -8,7 +8,7 @@ echo "Export the public key"
 gpg2 --armor --output /certs/public.gpg --export codeSigning 2> /dev/null
 
 echo "Copy an image with skopeo"
-skopeo copy docker://busybox:latest dir:busybox
+skopeo copy --override-os linux docker://docker.io/busybox:latest dir:busybox
 
 echo "standalone-sign"
 skopeo standalone-sign busybox/manifest.json registry.example.com/example/busybox codeSigning --output busybox.signature
